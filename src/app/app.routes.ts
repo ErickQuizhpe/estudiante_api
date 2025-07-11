8c5f922c57078a6b864d3a492f8d54f9fa921ed5
tmkvn <tm.kevin98@gmail.com>
2025-07-10 22:17:30 -0500

    Refactor app layout and routing structure; remove Navbar component and integrate MainLayout and AdminLayout for improved organization of routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,28 @@ import { Recipes } from './pages/recipes/recipes';
 import { AboutUs } from './pages/about-us/about-us';
 import { Contact } from './pages/contact/contact';
 import { Login } from './pages/login/login';
-import { AuthGuard } from './guards/auth.guard';
 import { AdminGuard } from './guards/admin.guard';
 import { AdminHome } from './pages/admin/admin-home/admin-home';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'login', component: Login },
-  { path: 'recetas', component: Recipes, canActivate: [AuthGuard] },
-  { path: 'nosotros', component: AboutUs },
-  { path: 'contacto', component: Contact },
-  { path: 'admin', component: AdminHome, canActivate: [AdminGuard] },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'recetas', component: Recipes },
+      { path: 'nosotros', component: AboutUs },
+      { path: 'contacto', component: Contact },
+      { path: 'login', component: Login },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminLayout,
+    children: [{ path: '', component: AdminHome }],
+    canActivate: [AdminGuard],
+  },
   { path: '**', redirectTo: '' },
 ];
